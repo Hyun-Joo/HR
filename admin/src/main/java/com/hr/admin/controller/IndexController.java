497252d8f9e6f0c200f94076601e43c22aa7b1c9
HYUNJOO <HYUNJOO@DESKTOP-BJ080PU>
2020-11-29 18:40:56 +0900

    타임리프 테스트

--- admin/src/main/java/com/hr/admin/controller/IndexController.java
+++ admin/src/main/java/com/hr/admin/controller/IndexController.java
@@ -1,8 +1,8 @@
 package com.hr.admin.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -12,7 +12,10 @@ public class IndexController {
 
 	
 	@RequestMapping(value= {"/"})
-	public String login() {
-		return "login.html";
+	public ModelAndView login() {
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("pageName", "Welcome to IN+");
+		mav.setViewName("login");
+		return mav;
 	}
 }
