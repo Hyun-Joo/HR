4d2a818fd44d05ce2060e0de68289f72446c1c75
HYUNJOO <HYUNJOO@DESKTOP-BJ080PU>
2020-12-05 15:43:36 +0900

    젖댄 - 널체크

--- admin/src/main/resources/static/js/common.js
+++ admin/src/main/resources/static/js/common.js
@@ -13,7 +13,22 @@ function commonAjax(paramData, url, fnCallBack){
 	.fail(function(xhr, status, errorThrown) {
 		alert('오류로 인해 처리되지 않았습니다.\n새로고침을 실행합니다.');
 		location.reload(true);
-		})
+	})
 	.always(function(xhr, status) {
 	});
+}
+
+function isEmpty(val){
+	if(val == null || (typeof val == 'string' && val.trim() == '') || val == undefined){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+function nvl(val){
+	if(isEmpty(val)){
+		val = "";
+	}
+	return val;
 }
